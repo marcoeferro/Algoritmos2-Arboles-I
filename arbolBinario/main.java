0a4563d6cbad56bfa8516dac6305ad7076dd1502
Marco Ferro <marcoeferro2@gmail.com>
2022-06-22 11:05:05 -0300

    eliminar buscar recorrer habilitados

--- arbolBinario/main.java
+++ arbolBinario/main.java
@@ -12,6 +12,7 @@ public class main{
             System.out.print("[1] Añadir Nodo \n");
             System.out.print("[2] Mostrar Arbol \n");
             System.out.print("[3] Eliminar Nodo \n");
+            System.out.print("[4] Recorrer pre orden in orden post orden \n");
             System.out.print("[0] Salir del programa \n");
             rta=input.nextInt();
             input.nextLine();
@@ -38,10 +39,27 @@ public class main{
                     
                     break;
                 case 2:
+                    String valor;
                     System.out.print("Ingrese el valor que desea Eliminar \n");
+                    valor = input.nextLine();
+                    arbol.eliminar(valor);
                     break;
                 case 3:
+                    String valor2;
                     System.out.print("Ingrese el valor que desea Buscar \n");
+                    valor2 = input.nextLine();
+                    arbol.buscarNodo(valor2);
+
+                    break;
+                case 4:
+                    
+                System.out.print("Recorrido en Orden :");
+                    ArbolBinario.inorden(arbol.raiz);
+                    System.out.print("Recorrido en pre Orden :");
+                    ArbolBinario.preorden(arbol.raiz);
+                    System.out.print("Recorrido en Post Orden :");
+                    ArbolBinario.postorden(arbol.raiz);
+                    
                     break;
                 case 0 :
                     condicion = 0;
